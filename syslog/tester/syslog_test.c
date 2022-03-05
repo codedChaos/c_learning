90a1321c22cec8f0a87536db6f0c04dfaab552c1
codedChaos <devopsmyth@gmail.com>
2022-03-05 09:42:26 -0500

    mod syslog_test.c
    * format spacing between blocks and statements
    * add brief comments over statements, blocks, and method calls

--- syslog/tester/syslog_test.c
+++ syslog/tester/syslog_test.c
@@ -10,19 +10,21 @@ notes:		null
 #include <stdio.h>
 #include <syslog.h>
 
-
 // Variable DEFs
 #define ERR_MSG_TEST2 "test syslog entry using C lang program `syslog_tester.c`"
 const char * const ERR_MSG_TEST1 = "test syslog entry using C lang program `syslog_tester.c`";
-
-
 
 // int main program
 int main(void) {
  	// open syslog
 	openlog("test", LOG_PID, LOG_LOCAL0);
-
+	
+	// write test message
 	syslog(LOG_ERR, ERR_MSG_TEST2);
+
+	// cleanup: close syslog
 	closelog();
+
+	// exit program
 	return 0;
 }
