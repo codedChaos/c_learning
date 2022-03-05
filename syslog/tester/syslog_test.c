396512b857c71923e405006aa88ce405dcf66c23
codedChaos <devopsmyth@gmail.com>
2022-03-05 09:39:56 -0500

    modify: syslog/tester/syslog_test.c
    CHANGELOG:
    	- add ERR_MSG_TEST2 using \#define
    	- change ERR_MSG_TEST1 to constant pointer type
    	- fix method calls openlog and closelog
    		- openLog() -> change to -> openlog()
    		- closeLog() -> change to -> closelog()

--- syslog/tester/syslog_test.c
+++ syslog/tester/syslog_test.c
@@ -1,9 +1,28 @@
+/* 
+app: 	 	syslog_tester.c
+lang:  		written in C lang
+author: 	codedChaos
+date:		03052022-092500EDT
+update:		n/a
+notes:		null
+*/
+
 #include <stdio.h>
 #include <syslog.h>
 
+
+// Variable DEFs
+#define ERR_MSG_TEST2 "test syslog entry using C lang program `syslog_tester.c`"
+const char * const ERR_MSG_TEST1 = "test syslog entry using C lang program `syslog_tester.c`";
+
+
+
+// int main program
 int main(void) {
-    openLog("test", LOG_PID, LOG_LOCAL0);
-    syslog(LOG_ERR, "waaaaaah");
-    closeLog();
-    return 0;
+ 	// open syslog
+	openlog("test", LOG_PID, LOG_LOCAL0);
+
+	syslog(LOG_ERR, ERR_MSG_TEST2);
+	closelog();
+	return 0;
 }
